d00f405815093cd57f7accaea7ed5ede597a5b2f
YuichiOta <rb19780519@gmail.com>
2023-06-19 11:31:05 +0900

    changed to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout"
 import Image from "next/image"
 
-const Contact = () => {
+export default function Contact () {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -80,5 +80,3 @@ const Contact = () => {
     </Layout>
   );
 };
-
-export default Contact
